5f3cfde444a167fc1d79a41416958552d5d80f2c
“mhatreapurva” <amhatre@syr.edu>
2022-11-29 10:54:51 -0500

    base commit

--- Tetris/src/main.py
+++ Tetris/src/main.py
@@ -1,36 +1,4 @@
-import sys
-import random
-#from prof_minimax import minimax
-sys.dont_write_bytecode = True #Don't maintain cache.
-from modules.Shape import *
-import numpy as np
-from modules.Grid import *
-from modules.minimax import *
+from modules import node
 
-if __name__ == "__main__":
-
-    state = Grid(6,4).GRID
-    T = Shape() #initialize shape object
-    #print(T.validPositions(shape = T.T,GRID = state))
-    #print(T.validGridStates(shape = T.T, GRID = state))
-    #x = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[3,3,3,0],[0,3,0,0]])
-    #T.plotState(GRID = np.flipud(x))
-    #print(state.shape)
-    while state is not None:
-        shape = random.choice([T.O,T.I,T.T,T.J,T.L,T.S,T.Z])
-        print(f"shape:\n{shape}")
-        
-        #print(state)
-        state, u = maxFunc(state,shape)
-        print(f"state: {state}") 
-        T.plotState(state)
-        print(f"New state:\n {state}")
-        print("\n\n\n NEW ITER")
-    
-
-    
-
-    
-    
-
-    
+tetris = node.Node(4,5)
+print(tetris.score)
---
+++ Tetris/src/modules/node.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Node:
+    
+    def __init__(self,r,c):
+        self.grid = np.zeros((r,c)) #generate grid.
+        self.score = 0
+        
+
+
+
+
+    
