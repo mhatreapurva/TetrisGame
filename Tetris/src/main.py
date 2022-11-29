3ba2033c7d9d9755a88f7da3884e75eb16507408
“mhatreapurva” <amhatre@syr.edu>
2022-11-29 14:17:52 -0500

    Known issue: avoids redundant moves.

--- Tetris/src/main.py
+++ Tetris/src/main.py
@@ -1,4 +1,11 @@
 from modules import node
+from modules import shapes
+import numpy as np
 
 tetris = node.Node(4,5)
 print(tetris.score)
+tetris.grid = np.array([[1., 1., 0., 0., 1.],[0., 0., 0., 0., 0.],[0., 0., 0., 0., 0.],[0., 0., 0., 0., 0.]])
+
+shape = shapes.Shape()
+print(shape.validPositions(shape.O,tetris.grid))
+shape.printGridStates(shape.O,tetris.grid)
---
+++ Tetris/src/modules/expectimax.py
@@ -0,0 +1,8 @@
+import numpy as np
+import node
+
+# Pass state into expectimax and it should be able to return the best moves for all the shapes.
+
+class Expectimax:
+    _ = ""
+
